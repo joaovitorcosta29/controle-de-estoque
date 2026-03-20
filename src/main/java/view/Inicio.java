c0c1a05c5acdb9f4db64436e64e4738195ebf888
joaovitorcosta29 <joaovitorcostalima81@gmail.com>
2026-03-20 17:16:58 -0300

    segunda-inicializacao

--- src/main/java/view/Inicio.java
+++ src/main/java/view/Inicio.java
@@ -56,6 +56,8 @@ public class Inicio extends javax.swing.JFrame {
         cadastraProdutos = new javax.swing.JButton();
         excluirProdutos = new javax.swing.JButton();
         logOut = new javax.swing.JButton();
+        telaInicial = new javax.swing.JButton();
+        excluirProdutos2 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaHistorico = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
@@ -94,33 +96,52 @@ public class Inicio extends javax.swing.JFrame {
             }
         });
 
+        telaInicial.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        telaInicial.setText("Tela Inicial");
+        telaInicial.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                telaInicialActionPerformed(evt);
+            }
+        });
+
+        excluirProdutos2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        excluirProdutos2.setText("Editar Produtos");
+        excluirProdutos2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                excluirProdutos2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(UsuarioAtual, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(cadastraProdutos, javax.swing.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-                            .addComponent(excluirProdutos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(logOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(0, 15, Short.MAX_VALUE)))
-                .addContainerGap())
+                .addGap(24, 24, 24)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(cadastraProdutos, javax.swing.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+                    .addComponent(excluirProdutos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(logOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(telaInicial, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(excluirProdutos2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(21, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(UsuarioAtual, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(25, 25, 25)
+                .addGap(19, 19, 19)
                 .addComponent(UsuarioAtual)
-                .addGap(139, 139, 139)
+                .addGap(138, 138, 138)
+                .addComponent(telaInicial, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
                 .addComponent(cadastraProdutos, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
+                .addGap(31, 31, 31)
                 .addComponent(excluirProdutos, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addComponent(excluirProdutos2, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(logOut, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(87, 87, 87))
@@ -214,6 +235,16 @@ public class Inicio extends javax.swing.JFrame {
         
     }//GEN-LAST:event_logOutActionPerformed
 
+    private void telaInicialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_telaInicialActionPerformed
+        // TODO add your handling code here:
+        new Inicio().setVisible(true); 
+        this.setVisible(false);
+    }//GEN-LAST:event_telaInicialActionPerformed
+
+    private void excluirProdutos2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_excluirProdutos2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_excluirProdutos2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -253,11 +284,13 @@ public class Inicio extends javax.swing.JFrame {
     private javax.swing.JLabel UsuarioAtual;
     private javax.swing.JButton cadastraProdutos;
     private javax.swing.JButton excluirProdutos;
+    private javax.swing.JButton excluirProdutos2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton logOut;
     private javax.swing.JTable tabelaHistorico;
+    private javax.swing.JButton telaInicial;
     // End of variables declaration//GEN-END:variables
 }
